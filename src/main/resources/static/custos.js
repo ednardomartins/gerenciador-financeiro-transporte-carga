f5da076062f87c4ce104bf3656a815ec6857d277
ednardo <ednardo@confeso.com.br>
2018-09-04 07:11:51 -0300

    bloquear formulario para textos.

--- src/main/resources/static/custos.js
+++ src/main/resources/static/custos.js
@@ -1,4 +1,24 @@
 var app = angular.module("GerenciadorFinanceiroTransporteCarga", []);
+
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9-]/g, '');
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
  
 app.controller("CustoTransporteCargaController", function($scope, $http) {
 	$scope.valor = "";
